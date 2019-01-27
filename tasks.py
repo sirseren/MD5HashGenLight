a8f4e4bb0ae8c02a653a64f1ff52355acdff4bc7
sirseren <46634550+sirseren@users.noreply.github.com>
2019-01-28 00:15:03 +0300

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -13,5 +13,3 @@ def calc_md5_hash_of_remote_file(url, email_to=''):
         send_result_to_email(email_to, str(url), hash_result)
     return hash_result
 
-
-#calc_md5_hash_of_remote_file("https://habrastorage.org/getpro/habr/post_images/346/c8c/f1c/346c8cf1c996a2065a3f4528f678b725.png")
